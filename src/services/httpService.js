2eabecd3214e21d6a3bf69162b7f447382042d1e
Yulin <1710285545@qq.com>
2022-04-08 11:50:04 +0800

    16-Extracted logger

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import logger from "./logService";
 import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, (error) => {
@@ -7,7 +8,7 @@ axios.interceptors.response.use(null, (error) => {
     error.response.status >= 400 &&
     error.response.status < 500;
   if (!expectedError) {
-    console.log("Logging the error", error);
+    logger.log(error);
     toast.error("An unexpected error occured");
   }
 
