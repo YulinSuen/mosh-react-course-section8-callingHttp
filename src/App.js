5421e4dcbc4ce0f9c687438c443f5d25e87595dc
Yulin <1710285545@qq.com>
2022-04-08 10:57:31 +0800

    12-Reusable httpService

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An unexpected error occured");
-  }
-
-  return Promise.reject(error);
-});
 
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts"; //fake backend
 class App extends Component {
@@ -24,14 +11,14 @@ class App extends Component {
   async componentDidMount() {
     // 用于装载异步操作对象，即将要完成的操作
     // pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
 
   // 先请求服务器，再进行前端修改，为保守(Pessimistic)操作
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -39,7 +26,7 @@ class App extends Component {
 
   handleUpdate = async (post) => {
     post.title = "UPDATE";
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    await http.put(apiEndpoint + "/" + post.id, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -54,7 +41,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete("s" + apiEndpoint + "/" + post.id);
+      await http.delete("s" + apiEndpoint + "/" + post.id);
     } catch (ex) {
       // Expected
       if (ex.response && ex.response.status === 404)
